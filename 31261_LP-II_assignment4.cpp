6ce7721a1362e11cc568bc8c80362cd3cca198d6
patilmehul <mr09patil@gmail.com>
2022-04-28 22:26:15 +0530

    31261_LP-II_assignment4

--- 31261_LP-II_assignment4.cpp
+++ 31261_LP-II_assignment4.cpp
@@ -3,84 +3,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class NQueens
-{
-    int N;
-    vector<vector<int>> board;
-
-public:
-    NQueens(int n)
-    {
-        N = n;
-        for (int i = 0; i < N; i++)
-        {
-            vector<int> a;
-            for (int j = 0; j < N; j++)
-            {
-                a.push_back(0);
-            }
-            board.push_back(a);
-        }
-    }
-
-    void printSolution()
-    {
-        for (int i = 0; i < N; i++)
-        {
-            for (int j = 0; j < N; j++)
-                printf(" %d ", board[i][j]);
-            printf("\n");
-        }
-    }
-
-    bool isSafe(int row, int col)
-    {
-        int i, j;
-
-        for (i = 0; i < col; i++)
-            if (board[row][i])
-                return false;
-
-        for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
-            if (board[i][j])
-                return false;
-
-        for (i = row, j = col; j >= 0 && i < N; i++, j--)
-            if (board[i][j])
-                return false;
-
-        return true;
-    }
-
-    bool solveNQUtil(int col)
-    {
-        if (col >= N)
-            return true;
-        for (int i = 0; i < N; i++)
-        {
-            if (isSafe(i, col))
-            {
-                board[i][col] = 1;
-
-                if (solveNQUtil(col + 1))
-                    return true;
-
-                board[i][col] = 0; // BACKTRACK
-            }
-        }
-        return false;
-    }
-    bool solveNQ()
-    {
-        if (solveNQUtil(0) == false)
-        {
-            printf("Solution does not exist");
-            return false;
-        }
-        printSolution();
-        return true;
-    }
-};
 
 class Solution {
 public:
@@ -140,12 +62,9 @@ public:
 
 int main()
 {
-    cout << "\nNQueens Problem using BackTracking approach: \n\n";
     int n;
     cout << "Enter the dimensions of the board: ";
     cin >> n;
-    NQueens nq = NQueens(n);
-    nq.solveNQ();
     Solution s;
     vector<vector<string>> a=s.solveNQueens(n);
     cout<<"All Possible Solutions= "<<a.size()<<endl;    
